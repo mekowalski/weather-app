6e575b55b77c948ee456276716341568479e4a9a
Malind Kowalski <malind.nikulski@gmail.com>
2018-12-26 12:47:13 -0700

    comments on App.js file/Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//Main file working, React COmponent
+import React from 'react' //import react object from react package in package.json
 
-class App extends Component {
+//this class creates an instance of App and extends React.Component that is an object that lives in
+//node_modules
+class App extends React.Component {
   render() {
     return (
+      //JSX compiled with Babel
+      //purpose of React is to have multiple files containing UI code
+      //and import those files into one single file
+      //then that one single file gets exported onto the main index.html
+      //lastly the main index.html is what you see in browser
       <div>
         <p>Hello Weather App Users</p>
       </div>
@@ -12,4 +18,5 @@ class App extends Component {
   }
 }
 
-export default App;
+//in order for other files to import this Component successfully, need to export this file at the bottom
+export default App //tells file that this Component is accessible
